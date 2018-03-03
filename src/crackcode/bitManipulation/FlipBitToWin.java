297eeb7ff611cfa8f5afb57b16462e0aa73c2c9d
bommr1 <rakesh.bommisetty@in.pega.com>
2018-03-03 08:42:19 +0530

    modified

--- src/crackcode/bitManipulation/FlipBitToWin.java
+++ src/crackcode/bitManipulation/FlipBitToWin.java
@@ -9,7 +9,15 @@ public class FlipBitToWin {
 		//System.out.println("index to replace for getting longest seq of 1's::"+index);
 		
 		//System.out.println(solutionGiven(Integer.parseInt("1101111011110111",2)));
-		System.out.println(solutionGiven(Integer.parseInt("1",2)));
+		/*System.out.println(~0);
+		i =Integer.parseInt("1111111111111111111111111111111",2);
+		System.out.println(i);
+		System.out.println(solutionGiven(-1));*/
+		System.out.println("11011001110111100110111");
+	//	System.out.println(solutionGiven(Integer.parseInt("11011001110111100110111",2)));
+		System.out.println(FlipBit("11011001110111100110111"));
+
+	
 	}
 
 	private static int solutionGiven(int a) {
@@ -17,17 +25,21 @@ public class FlipBitToWin {
 		
 			/* If all ls, this is already the longest sequence. */
 		int b = ~a;
+	//	System.out.println(Integer.toBinaryString(b));
 		
 			if (~a== 0) return Integer.BYTES * 8;
 
 			int currentlength = 0;
 			int previouslength = 0;
-			int maxlength = 1; // We can always have a s equence of at least one 1
+			int maxlength = 0; // We can always have a s equence of at least one 1
 			while (a!= 0) {
+			//	System.out.println(Integer.toBinaryString(a&1));
 			if ((a & 1) == 1) { // Current bit is a 1
+				
 			 currentlength++;
 			 } else if ((a & 1) == 0) { // Current bit is a 0
 			 /* Update to 0 (if next bit is 0) or currentlength (if next bit is 1). */
+				// System.out.println(Integer.toBinaryString(a&2));
 			 previouslength = (a & 2) == 0? 0 : currentlength;
 			 currentlength = 0;
 			 }
@@ -42,51 +54,25 @@ public class FlipBitToWin {
 
 	private static int FlipBit(String binaryString) {
 		
-		binaryString = "1101110011101111";
-		 int primary = 0, secondary=0,total=0,tindex, count=0,pindex=-1,sindex=-1;
+		//binaryString = "1101110011101111";
+		 int primary = 0, secondary=0,total=0;
 		 
 		 for(int i=0;i<binaryString.length();++i){
 			 if(binaryString.charAt(i)=='1'){
-				 count++;
+				 primary++;
 			 }else{
-				 if(binaryString.charAt(i+1)=='1'){
-					 if(primary ==0){
-						 primary = count;
-						 pindex=i;
-						 count=0;
-						 
-					 }else if(primary!=0 && secondary==0){
-						 secondary = count;
-						 sindex=i;
-						 count=0;
-					 }
-					 else if(count > primary && secondary!=0){
-						 primary = secondary;
-						 secondary = count;
-						 pindex=sindex;
-						 count=0;
-					 }
-				 }else{
-					 if(total < primary + secondary && secondary!=0){
-						 total = primary + secondary;
-						 tindex = pindex;
-						 primary = secondary =count=0;
-					 }else{
-						 primary = secondary =count=0;
-					 }
-				 }
-				
+				 if(i+1 < binaryString.length() && binaryString.charAt(i+1)=='1')
+					 secondary =primary;
+					 primary =0;
 			 }
+			 
+			 total = Math.max(primary + secondary + 1, total);
+			 
 		 }
 		 
-		 if(count > primary){
-			 primary = secondary;
-			 secondary = count;
-			 pindex=sindex;
-			 count=0;
-		 }
 		
-		 return pindex;
+		
+		 return total;
 		
 		
 	}
